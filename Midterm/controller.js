9b4a84227d1aeef8ba27a1468d28405ae1998003
Xiaoxuan Zhang <zhan5472@umn.edu>
2019-10-26 18:47:59 -0500

    Oct 26

---
+++ Midterm/controller.js
@@ -0,0 +1,55 @@
+function creataList(){
+  var button = document.querySelector("#creatList");
+  button.style.display = "none";
+  var input = document.querySelector("#inputTitle");
+  var button = document.querySelector("#titleSaver");
+  document.querySelector("#enterTitle").style.display="inline";
+  input.style.display = "inline";
+  button.style.display = "inline";
+}
+function saveTitle(){
+  var input = document.querySelector("#inputTitle");
+  var parent = document.querySelector("#main");
+  var inputVal = input.value;
+  var mytitle = document.querySelector("#mytitle");
+  var saver = document.querySelector("#titleSaver")
+  mytitle.innerHTML = inputVal;
+  document.querySelector("#searchBar").style.display = "inline";
+  document.querySelector("#searchButton").style.display = "inline";
+  document.querySelector("#addproduct").style.display = "inline";
+  var changebutton = document.querySelector("#changetitle");
+  changebutton.style.display ="inline";
+  saver.style.display ="none";
+  input.style.display="none";
+  document.querySelector("#enterTitle").style.display="none";
+  document.querySelector("#share").style.display="block"
+
+}
+function changeTitle(){
+  var input = document.querySelector("#inputTitle");
+  var saver = document.querySelector("#titleSaver")
+  var mytitle = document.querySelector("#mytitle");
+  mytitle.innerHTML = "";
+  input.style.display="inline";
+  saver.style.display="inline";
+  document.querySelector("#enterTitle").style.display="inline";
+  var changebutton = document.querySelector("#changetitle");
+  changebutton.style.display ="none";
+
+}
+function addproduct(){
+  var videoContainer = document.querySelector("#video-container");
+  videoContainer.innerHTML="";
+  var product = document.createElement("LI");
+  product.innerHTML = document.querySelector("#searchBar").value+"      ";
+  var list = document.getElementById("wishList");
+  list.appendChild(product);
+  var delButton = document.createElement("button");
+  delButton.className = "delete";
+  delButton.innerHTML ="delete this product";
+  delButton.addEventListener("click", function(){
+    var parent = this.parentElement;
+    parent.removeChild(this);
+  }.bind(product));
+  product.appendChild(delButton);
+}
---
+++ Midterm/google.js
@@ -0,0 +1,103 @@
+function authenticate() {
+  return gapi.auth2.getAuthInstance()
+      .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send"})
+      .then(function() { console.log("Sign-in successful");
+      updateSigninStatus(true)},
+            function(err) { console.error("Error signing in", err); });
+}
+
+function loadClient() {
+  gapi.client.setApiKey("AIzaSyBM-0pKFORZ6oHvG71mZftMHTcXzLmBtw4");
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+      .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+}
+function loadClient2(){
+  gapi.client.setApiKey("AIzaSyBM-0pKFORZ6oHvG71mZftMHTcXzLmBtw4");
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest")
+      .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function search() {
+  var keyword = document.querySelector("#searchBar").value;
+  var main = document.querySelector("#main");
+  return gapi.client.youtube.search.list({
+    "part": "snippet",
+    "order": "relevance",
+    "q": `${keyword}`,
+    "type": "video"
+  })
+      .then(function(response) {
+            console.log("Response",response);
+            var output =`<h4 style="center-align;">Related Videos<h4>`;
+            var container = document.querySelector("#video-container");
+            var counter = 1;
+            for (item of response.result.items){
+              var id = item.id.videoId;
+              var url = "https://www.youtube.com/embed/"+id;
+              var title = item.snippet.title;
+              output +=`<div style="font:Bebas Neue;">
+              <div>${counter}.${title}</div>
+                        <iframe width="auto" height="auto" src=${url}
+                        <frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>`
+              counter=counter+1;
+            }
+            console.log("success")
+            container.innerHTML = output;
+            },
+            function(err) { console.error("Execute error", err); alert("error!")});
+}
+gapi.load("client:auth2", function() {
+  gapi.auth2.init({client_id: "411935288663-s3rfe141sd25hqecu4qonocjp37arpon.apps.googleusercontent.com"});
+  updateSigninStatus(false);
+});
+
+function updateSigninStatus(isSignedIn) {
+        var authorizeButton =  document.querySelector("#loginButton");
+        var signoutButton = document.querySelector("#logoutButton")
+        if (isSignedIn) {
+          authorizeButton.style.display = 'none';
+          signoutButton.style.display = 'block';
+        } else {
+          authorizeButton.style.display = 'block';
+          signoutButton.style.display = 'none';
+        }
+      }
+function signOut() {
+          var auth2 = gapi.auth2.getAuthInstance();
+          auth2.signOut().then(function () {
+            console.log('User signed out.');
+            updateSigninStatus(false);
+          });
+        }
+function sendMessage() {
+          // Using the js-base64 library for encoding:
+          var email = document.querySelector("#emailAdress").value;
+          var status = document.querySelector("#status")
+          var output = "";
+          var order = 1;
+          var wish_list = document.querySelector("#wishList");
+          for(each of wish_list.children){
+            output+= `${order}. ${each.innerHTML.split(" ")[0]}\n`;
+            order+=1;
+          }
+          var title = document.querySelector("#mytitle").innerHTML;
+          const message =
+          `To: ${email}\r\n` +
+          `Subject: ${title} wishList\r\n\r\n` +
+          output;
+
+
+          // The body needs to be base64url encoded.
+const encodedMessage = btoa(message)
+
+const readedEncodedMessage = encodedMessage.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+gapi.client.gmail.users.messages.send({
+    'userId': 'me',
+    'resource': {
+      'raw': readedEncodedMessage
+              }
+          }).then(function () {status.innerHTML = "successfully send to: "+email});
+}
